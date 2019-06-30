165213de4c8f913a7cfd4877929e60e191672337
nikfarjam <mm.nikfarjam@gmail.com>
2019-06-30 10:11:08 +1000

    Add logger

--- src/main/java/com/mehdi/optimizedbasket/App.java
+++ src/main/java/com/mehdi/optimizedbasket/App.java
@@ -1,8 +1,14 @@
 package com.mehdi.optimizedbasket;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
 
+    private static final Logger logger = LogManager.getLogger("com.mehdi.optimizedbasket.App");
+
     public static void main(String[] args) {
-        
+
+        logger.debug("Start");
     }
 }

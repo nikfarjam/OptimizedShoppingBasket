8639a0fc71fd5a3ece9fc0df721b6a366c1de5a7
nikfarjam <mm.nikfarjam@gmail.com>
2019-06-30 12:27:41 +1000

    Implement Greedy algorithm
    Implement tests

--- src/main/java/com/mehdi/optimizedbasket/service/GreedyPickerImpl.java
+++ src/main/java/com/mehdi/optimizedbasket/service/GreedyPickerImpl.java
@@ -1,20 +1,45 @@
 package com.mehdi.optimizedbasket.service;
 
 import com.mehdi.optimizedbasket.model.Item;
+import com.mehdi.optimizedbasket.util.Utils;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class GreedyPickerImpl implements ItemPicker {
 
-    @Override
-    public BigDecimal calcItemPriority(Item item) {
-        return null;
-    }
+    private Comparator<Item> sortDescByWeight = (i1, i2) -> {
+        BigDecimal weight1 = calcItemPriority(i1);
+        BigDecimal weight2 = calcItemPriority(i2);
+        return weight2.compareTo(weight1);
+    };
 
     @Override
     public Optional<Basket> selectItems(List<Item> items, BigDecimal priceLimit) {
-        return Optional.empty();
+        if (priceLimit == null || priceLimit.compareTo(BigDecimal.ZERO) == 0) {
+            return Optional.empty();
+        }
+        if (Utils.isEmptyCollection(items)) {
+            return Optional.empty();
+        }
+        final Basket basket = new Basket(priceLimit);
+        Optional<Item> overFlowItem = items.stream()
+                .sorted(sortDescByWeight)
+                .filter(i -> !basket.addItem(i))
+                .findFirst();
+        if (overFlowItem.isPresent()) {
+            return Optional.of(basket);
+        }
+        return Optional.of(basket);
     }
+
+    @Override
+    public BigDecimal calcItemPriority(Item item) {
+        BigDecimal cost = item.getShippingCost().add(item.getPrice());
+        return BigDecimal.valueOf(item.getRating()).divide(cost, 4, RoundingMode.HALF_DOWN);
+    }
+
 }
